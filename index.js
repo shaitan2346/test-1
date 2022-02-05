fc426703f4a975123f48929ffd02f7d58b5baa20
Petr <ermol19922346@gmaile.com>
2022-02-05 20:30:49 +0300

    Added hq tag

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-// импортируем функцию plusOne
-const { plusOne,  sum } = require('./module-1/functions');
+// импортируем функции
+const { plusOne, sum, sub, mult, div } = require('./module-1/functions');
 
-console.log('Это сообщение скинул со своего компа вместо "Привет"!');
+console.log('Ай Вэ, Братан');
+console.log('');
 
-const a = 5, b = 7;
+const a = 69, b = 3;
 
-console.log('plusOne(', a, ') = ' , plusOne(a));
+console.log('plusOne(', a, ') = ', plusOne(a));
+console.log('sum(', a, ',', b, ' ) = ', sum(a, b));
+console.log('sub(', a, ',', b, ' ) = ', sub(a, b));
+console.log('mult(', a, ',', b, ' ) = ', mult(a, b));
+console.log('div(', a, ',', b, ' ) = ', div(a, b));
 
-console.log('sum(', a, ',', b, ' ) = ' , sum(a, b));
--- module-1/functions.js
+++ module-1/functions.js
@@ -1,49 +1,89 @@
-function plusOne(num) {
-    return num + 1;
+// Функция + 1
+function plusOne(arg) {
+    return arg + 1;
+}
+//Вычитание (Работает с положительными и отрицательными аргументами)
+function sub(a, b) {
+    let c = 0;
+    if (a > b) {
+        while (b < a) {
+            b = plusOne(b);
+            c = plusOne(c);
+        }
+        return c;
+    }
+    else if (a < b) {
+        while (a < b) {
+            a = plusOne(a);
+            c = plusOne(c)
+        }
+        return -c;
+    }
 }
 
-/*
-    x - number,
-    y - number,
-    return x + y
-*/
-// нельзя использовать: +, -, *, /, %
-function sum(x, y) {
-    let result = x;
-    let i = 1;
-
-    while (i <= y) {
-        result = plusOne(result);
-        i = plusOne(i);
+//Сложение (Работает с положительными и отрицательными аргументами)
+function sum(a, b) {
+    let c = 0;
+    if (c < a, c < b) {
+        while (c < b) {
+            c = plusOne(c);
+            a = plusOne(a);
+        }
+        return a;
+    }
+    else if (c > a, c > b) {
+        a = -a;
+        b = -b;
+        while (c < b) {
+            c = plusOne(c);
+            (a) = plusOne(a);
+        }
+        return -a;
+    }
+    else if (c > a, c < b) {
+        a = -a;
+        while (a < b) {
+            a = plusOne(a);
+            c = plusOne(c);
+        }
+        return c;
     }
 
-    return result;
 }
-
-/*
-    x - number,
-    y - number,
-    return x - y
-*/
-function sub(x, y);
-
-/*
-    x - number,
-    y - number,
-    return x * y
-*/
-function mul(x, y);
-
-/*
-    x - number,
-    y - number,
-    return x / y
-*/
-function div(x, y);
-
-
-// экспортируем функции:
+// Умножение (Работает с положительными и отрицательными аргументами)
+function mult(a, b) {
+    let d = 0;
+    let c = 0;
+    if (d < a) {
+        while (d < a) {
+            d = plusOne(d);
+            c = sum(c, b);
+        }
+        return c;
+    }
+    else if (d > a) {
+        a = -a;
+        while (d < a) {
+            d = plusOne(d);
+            c = sum(c, b);
+        }
+        return -c;
+    }
+}
+//Деление (Работает только с положительными аргументами)
+function div(a, b) {
+    let c = 0;
+    let d = -1;
+    while (c <= a) {
+        d = plusOne(d);
+        c = sum(c, b);
+    }
+    return d;
+}
 module.exports = {
     plusOne: plusOne,
-    sum: sum
-};
+    sum: sum,
+    sub: sub,
+    mult: mult,
+    div: div
+}
